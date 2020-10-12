c9544eee46816b5a13bc1f40fcecbca6b53294e3
wonsunn <47625368+wonsunn@users.noreply.github.com>
2020-10-12 16:32:46 +0900

    단어 섞기 - BFS, 문자열

---
+++ BOJ/BOJ_9177.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    private static class Info {
+        String str;
+        int posStr, posF, posS;
+
+        Info (String str, int posStr, int posF, int posS) {
+            this.str = str;
+            this.posStr = posStr;
+            this.posF = posF;
+            this.posS = posS;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String first = st.nextToken();
+            String second = st.nextToken();
+            String third = st.nextToken();
+
+            if (first.concat(second).equals(third) || second.concat(first).equals(third)) {
+                System.out.println("Data set " + i + ": yes");
+                continue;
+            }
+
+            boolean[][] visited = new boolean[first.length() + 1][second.length() + 1];
+
+            Queue<Info> q = new LinkedList<>();
+            q.add(new Info("", 0, 0, 0));
+
+            boolean yes = false;
+            while (!q.isEmpty()) {
+                String str = q.peek().str;
+                int posStr = q.peek().posStr;
+                int posF = q.peek().posF;
+                int posS = q.peek().posS;
+
+                q.poll();
+
+                if (str.equals(third) && posStr == third.length()) {
+                    yes = true;
+                    break;
+                }
+
+                if (posF < first.length() && !visited[posF + 1][posS] && first.charAt(posF) == third.charAt(posStr)) {
+                    q.add(new Info(str.concat(String.valueOf(first.charAt(posF))), posStr + 1, posF + 1, posS));
+                    visited[posF + 1][posS] = true;
+                }
+                if (posS < second.length() && !visited[posF][posS + 1] && second.charAt(posS) == third.charAt(posStr)) {
+                    q.add(new Info(str.concat(String.valueOf(second.charAt(posS))), posStr + 1, posF, posS + 1));
+                    visited[posF][posS + 1] = true;
+                }
+            }
+
+            if (yes) System.out.println("Data set " + i + ": yes");
+            else System.out.println("Data set " + i + ": no");
+        }
+    }
+}

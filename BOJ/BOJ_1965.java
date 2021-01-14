97b47ae5d4a8416dfcf9b917f37536dc336c2f9e
wonsunn <47625368+wonsunn@users.noreply.github.com>
2021-01-15 00:14:14 +0900

    상자넣기 - dp

---
+++ BOJ/BOJ_1965.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int max = 0;
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
+            }
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+    }
+}

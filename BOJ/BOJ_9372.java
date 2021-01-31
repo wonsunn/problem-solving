c502433de4a5d574adc3221d8c5daadd0e040d42
wonsunn <47625368+wonsunn@users.noreply.github.com>
2021-01-31 14:16:33 +0900

    상근이의 여행 - 구현

---
+++ BOJ/BOJ_9372.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+            }
+            System.out.println(n - 1);
+        }
+    }
+}
